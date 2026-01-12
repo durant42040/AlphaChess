696b5b6f520ed7449b33c433632e5312ff771a34
durant42040 <b11901040@ntu.edu.tw>
2026-01-12 23:05:11 +0800

    add more tests

--- engine/tests/perft.rs
+++ engine/tests/perft.rs
@@ -26,3 +26,41 @@ fn test_kiwipete_perft() {
         assert_eq!(nodes, results[(depth - 1) as usize]);
     }
 }
+
+#[test]
+fn test_perft_3() {
+    let results = vec![14, 191, 2812, 43238, 674624];
+
+    let mut engine = Engine::new();
+    engine.load_from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1".to_string());
+    for depth in 1..6 {
+        let nodes = engine.perft(depth);
+        assert_eq!(nodes, results[(depth - 1) as usize]);
+    }
+}
+
+#[test]
+fn test_perft_4() {
+    let results = vec![6, 264, 9467, 422333];
+
+    let mut engine = Engine::new();
+    engine.load_from_fen(
+        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1".to_string(),
+    );
+    for depth in 1..5 {
+        let nodes = engine.perft(depth);
+        assert_eq!(nodes, results[(depth - 1) as usize]);
+    }
+}
+
+#[test]
+fn test_perft_5() {
+    let results = vec![44, 1486, 62379, 2103487];
+
+    let mut engine = Engine::new();
+    engine.load_from_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8".to_string());
+    for depth in 1..5 {
+        let nodes = engine.perft(depth);
+        assert_eq!(nodes, results[(depth - 1) as usize]);
+    }
+}
