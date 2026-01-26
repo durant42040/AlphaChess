07797d30e2da525e256a78e5ed89a0a223754053
durant42040 <b11901040@ntu.edu.tw>
2026-01-26 22:50:26 +0800

    minimax

--- engine/src/search/mod.rs
+++ engine/src/search/mod.rs
@@ -1,79 +1,60 @@
 pub mod perft;
 
+use std::cmp::max;
+
 pub use perft::Perft;
 
-use crate::{Engine, chess::Move, engine::Evaluation};
+use crate::engine::{Engine, Evaluation};
 
 pub trait Search {
     /// Find the best sequence of moves up to the given depth
-    fn search(&mut self, depth: u8) -> Option<(Vec<Move>, i32)>;
+    fn max_search(&mut self, depth: u8) -> i32;
+    fn minimax_search(&mut self, depth: u8) -> i32;
 }
 
 impl Search for Engine {
-    /// find a sequence of moves, evaluate score, return best sequence of moves
-    fn search(&mut self, depth: u8) -> Option<(Vec<Move>, i32)> {
-        if self.get_game_state() != "playing" {
-            return None;
-        }
-
-        let mut searcher = State::new();
-        searcher.dfs(self, depth);
-
-        if searcher.best_moves.is_empty() {
-            None
-        } else {
-            assert_eq!(searcher.best_moves.len(), depth as usize);
-            Some((searcher.best_moves, searcher.best_score))
-        }
-    }
-}
-
-/// Helper struct to hold search state
-struct State {
-    curr_moves: Vec<Move>,
-    best_moves: Vec<Move>,
-    best_score: i32,
-}
-
-impl State {
-    fn new() -> Self {
-        Self {
-            curr_moves: Vec::new(),
-            best_moves: Vec::new(),
-            best_score: i32::MIN,
-        }
-    }
-
-    /// depth-first search
-    fn dfs(&mut self, engine: &mut Engine, depth: u8) {
+    fn max_search(&mut self, depth: u8) -> i32 {
         if depth == 0 {
-            let score = engine.eval();
-            if score > self.best_score {
-                self.best_score = score;
-                self.best_moves = self.curr_moves.clone();
-            }
-            return;
+            let score = self.eval();
+            return score;
         }
 
-        let moves = engine.generate_all_legal_moves();
+        let mut score = i32::MIN;
+
+        let moves = self.generate_all_legal_moves();
         for r#move in moves {
-            engine.board_mut().act(r#move);
-            engine.update_game_state();
-            self.curr_moves.push(r#move);
-            self.dfs(engine, depth - 1);
-            self.curr_moves.pop();
-            engine.undo();
+            self.board_mut().act(r#move);
+            self.update_game_state();
+            score = max(score, self.max_search(depth - 1));
+            self.undo();
         }
+
+        score
+    }
+
+    fn minimax_search(&mut self, depth: u8) -> i32 {
+        if depth == 0 {
+            let score = self.eval();
+            return score;
+        }
+
+        let mut score = i32::MIN;
+
+        let moves = self.generate_all_legal_moves();
+        for r#move in moves {
+            self.board_mut().act(r#move);
+            self.update_game_state();
+            score = max(score, -self.minimax_search(depth - 1));
+            self.undo();
+        }
+
+        score
     }
 }
 
 #[test]
 fn test_search() {
     let mut engine = Engine::new();
-    if let Some((best_moves, best_score)) = engine.search(5) {
-        for r#move in best_moves {
-            println!("{}", r#move);
-        }
-        println!("{}", best_score);
-    }
+    let best_score = engine.max_search(5);
+    println!("{}", best_score);
 }
