8b21f5d5b7834b114d53745fe58f528ccdcab916
durant42040 <b11901040@ntu.edu.tw>
2026-02-07 23:45:46 +0800

    fmt

--- engine/src/search/eval.rs
+++ engine/src/search/eval.rs
@@ -1,6 +1,10 @@
 use crate::chess::{Bitboard, Piece};
 use crate::constants::{
-    ATTACK_WEIGHT, BLACK_BISHOP_SCORE, BLACK_KING_SCORE, BLACK_KING_ZONE, BLACK_PASSED_MASK, BLACK_PAWN_SCORE, BLACK_ROOK_SCORE, DOUBLE_PAWN_PENALTY, FILE_MASKS, ISOLATED_MASK, ISOLATED_PAWN_PENALTY, KING_SHIELD_BONUS, KNIGHT_SCORE, OPEN_FILE_BONUS, PASSED_PAWN_BONUS, RANK_7_BONUS, SEMI_OPEN_FILE_BONUS, WHITE_BISHOP_SCORE, WHITE_KING_SCORE, WHITE_KING_ZONE, WHITE_PASSED_MASK, WHITE_PAWN_SCORE, WHITE_ROOK_SCORE
+    ATTACK_WEIGHT, BLACK_BISHOP_SCORE, BLACK_KING_SCORE, BLACK_KING_ZONE, BLACK_PASSED_MASK,
+    BLACK_PAWN_SCORE, BLACK_ROOK_SCORE, DOUBLE_PAWN_PENALTY, FILE_MASKS, ISOLATED_MASK,
+    ISOLATED_PAWN_PENALTY, KING_SHIELD_BONUS, KNIGHT_SCORE, OPEN_FILE_BONUS, PASSED_PAWN_BONUS,
+    RANK_7_BONUS, SEMI_OPEN_FILE_BONUS, WHITE_BISHOP_SCORE, WHITE_KING_SCORE, WHITE_KING_ZONE,
+    WHITE_PASSED_MASK, WHITE_PAWN_SCORE, WHITE_ROOK_SCORE,
 };
 use crate::{
     Engine,
@@ -74,19 +78,17 @@ impl Evaluation for Engine {
 
     fn double_pawn_penalty(&self) -> i32 {
         let mut score = 0;
-        
+
         for file in &FILE_MASKS {
-            let white_pawns = self.pieces().white_pieces()
-                & self.pieces().pawns()
-                & Bitboard::from(*file);
+            let white_pawns =
+                self.pieces().white_pieces() & self.pieces().pawns() & Bitboard::from(*file);
             let white_pawns_count = white_pawns.count();
             if white_pawns_count > 1 {
                 score += DOUBLE_PAWN_PENALTY * (white_pawns_count - 1) as i32;
             }
 
-            let black_pawns = self.pieces().black_pieces()
-                & self.pieces().pawns()
-                & Bitboard::from(*file);
+            let black_pawns =
+                self.pieces().black_pieces() & self.pieces().pawns() & Bitboard::from(*file);
             let black_pawns_count = black_pawns.count();
             if black_pawns_count > 1 {
                 score -= DOUBLE_PAWN_PENALTY * (black_pawns_count - 1) as i32;
@@ -173,11 +175,21 @@ impl Evaluation for Engine {
         let black_king = black_pieces & self.pieces().kings();
         let white_pawns = white_pieces & self.pieces().pawns();
         let black_pawns = black_pieces & self.pieces().pawns();
-        
+
         // The king shield is the number of friendly pieces near the king
-        score += (self.move_generator.generate_king_moves(Square::from(white_king)) & self.pieces().white_pieces()).count() as i32 * KING_SHIELD_BONUS;
-        score -= (self.move_generator.generate_king_moves(Square::from(black_king)) & self.pieces().black_pieces()).count() as i32 * KING_SHIELD_BONUS;
-        
+        score += (self
+            .move_generator
+            .generate_king_moves(Square::from(white_king))
+            & self.pieces().white_pieces())
+        .count() as i32
+            * KING_SHIELD_BONUS;
+        score -= (self
+            .move_generator
+            .generate_king_moves(Square::from(black_king))
+            & self.pieces().black_pieces())
+        .count() as i32
+            * KING_SHIELD_BONUS;
+
         // semi-open and open file penalties
         let square = Square::from(white_king);
         let file_mask = Bitboard::from(FILE_MASKS[square.file as usize]);
@@ -197,10 +209,10 @@ impl Evaluation for Engine {
         }
 
         // king zone attacks
-        let white_king_zone = Bitboard::from(WHITE_KING_ZONE[white_king.get_lsb() as usize]);     
+        let white_king_zone = Bitboard::from(WHITE_KING_ZONE[white_king.get_lsb() as usize]);
         let mut attack_count = 0;
         let mut attack_value = 0;
-        
+
         for square in (black_pieces & !black_king & !black_pawns).iter() {
             let moves = self.generate_moves(Square::from(square));
             if white_king_zone.intersects(moves) {
@@ -210,7 +222,7 @@ impl Evaluation for Engine {
             }
         }
         score -= attack_value * ATTACK_WEIGHT[attack_count] / 100;
-        
+
         let black_king_zone = Bitboard::from(BLACK_KING_ZONE[black_king.get_lsb() as usize]);
         attack_count = 0;
         attack_value = 0;
